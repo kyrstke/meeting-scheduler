47586f651961766f2892136d9ccb6a89e4b67928
Bambo <kyrstke@gmail.com>
2023-03-14 13:56:47 +0100

    Initialized svelte app and configured Firebase app

---
+++ meeting-scheduler/src/Firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgmKqMa4jtv9lY1hSwW4NpuT-oF99CzhQ",
+  authDomain: "meeting-scheduler-5beff.firebaseapp.com",
+  projectId: "meeting-scheduler-5beff",
+  storageBucket: "meeting-scheduler-5beff.appspot.com",
+  messagingSenderId: "236952680679",
+  appId: "1:236952680679:web:4935fefa114c1fdcfe4a42"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
