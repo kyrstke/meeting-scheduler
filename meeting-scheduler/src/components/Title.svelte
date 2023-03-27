77f330ac24b93a6b16cfa6740ec25396fc019914
Bambo <kyrstke@gmail.com>
2023-03-27 11:20:00 +0200

    Added close button to additional ranges, datepicker not working yet

--- meeting-scheduler/src/components/Title.svelte
+++ meeting-scheduler/src/components/Title.svelte
@@ -8,7 +8,7 @@
 <section class="bg-white dark:bg-gray-900">
   <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 flex justify-center">
     <div class="text-center">
-      <Heading tag="h2" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">Meeting Scheduler</Heading>
+      <Heading tag="h1" class="mb-4">Meeting Scheduler</Heading>
       <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">The best app for scheduling meetings with your team, clients and friends.</P>
     </div>
   </div>
--- meeting-scheduler/src/routes/+page.svelte
+++ meeting-scheduler/src/routes/+page.svelte
@@ -2,7 +2,8 @@
 <script lang="ts">
 
     import Title from '../components/Title.svelte';
-    import { Label, Input, Button, ButtonGroup, Checkbox, A, Datepicker, Heading, P } from 'flowbite-svelte';
+    import { Label, Input, Button, ButtonGroup, Datepicker, Heading, P } from 'flowbite-svelte';
+    import { XMark } from 'svelte-heros';
 
     let ranges_no = 0;
 
@@ -39,7 +40,7 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="text-center flex flex-wrap justify-center">
-            <Heading tag="h2" class="mb-4" customSize="text-3xl font-extrabold  md:text-4xl lg:text-5xl">Create an event</Heading>
+            <Heading tag="h2" class="mb-4">Create an event</Heading>
             <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Find the best time for your meeting quicker than ever before</P>
         </div>
         <div class="flex justify-center">
@@ -50,19 +51,21 @@
                 </div>
                 <div class="mb-6">
                     <Label for="datepicker" class="mb-2">Range</Label>
-                    <Datepicker range />
+                    <Datepicker range />  
                 </div>
                 {#each Array(ranges_no) as _, i}
                     <div class="mb-6">
-                        <Label class="mb-2">Range {i + 2}</Label>
+                        <div class="flex justify-between">
+                            <Label for="datepicker" class="mb-2">Range {i + 2}</Label>
+                            <button class="text-gray-100 dark:text-gray-400" on:click={() => {ranges_no--}}><XMark></XMark></button>
+                        </div>
                         <Datepicker range/>
                     </div>
                 {/each}
-                <Checkbox class="mb-6 space-x-1" required>I agree with the <A href="/">terms and conditions</A>.</Checkbox>
-                <ButtonGroup class="space-x-px">
-                    <Button pill color="blue" type="button" on:click={add_range}>Add another range</Button>
-                    <Button pill color="blue" type="submit">Submit</Button>
-                </ButtonGroup>
+                <div class="mt-8 flex justify-between">
+                    <Button color="alternative" type="button" on:click={add_range}>Add another range</Button>
+                    <Button type="submit">Next</Button>
+                </div>
             </form>
         </div>
     </div>
