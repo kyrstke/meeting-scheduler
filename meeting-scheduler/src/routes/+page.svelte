5e58ee310b23fa497709f01efdab652e9c9e9ca1
Bambo <kyrstke@gmail.com>
2023-04-20 22:49:26 +0200

    Added panel with hours, hidden button to add ranges

--- meeting-scheduler/src/routes/+page.svelte
+++ meeting-scheduler/src/routes/+page.svelte
@@ -2,40 +2,49 @@
 <script lang="ts">
 
     import Title from '../components/Title.svelte';
-    import { Label, Input, Button, ButtonGroup, Datepicker, Heading, P } from 'flowbite-svelte';
+    import { Label, Input, Button, Datepicker, Heading, P, CloseButton } from 'flowbite-svelte';
     // import { XMark } from 'svelte-heros';
 
-    // let ranges_no = 0;
+    let ranges_no = 0;
+    let start_hour = 9;
+    let end_hour = 22;
 
-    // function add_range() {
-    //     ranges_no++;
-    //     console.log(ranges_no);
-    // }
+    const handleNextButton = () => {
+        console.log('Next');
+    }
+
+    const arrayRange = Array.from(
+            { length: (end_hour - start_hour) },
+            (_, index) => start_hour + index
+        );
 
 </script>
 
 <Title />
 
-<!-- <section class="bg-white dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <Input>
-        <div class="flex flex-wrap flex-col m-4 hidden">
-            <div class="panel"></div>
-            <div class="panel"></div>
-            <div class="panel"></div>
-            <div class="panel"></div>
-            <div class="panel"></div>
-            <div class="panel"></div>
-            <div class="panel"></div>
-            <div class="panel"></div>
-            <div class="panel"></div>
-            <div class="panel"></div>
-            <div class="panel"></div>
-            <div class="panel"></div>
-            <div class="panel"></div>
+<section class="bg-white dark:bg-gray-900">
+    <div class="flex py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="-mt-4 mr-1">
+            {#each arrayRange as hour}
+                <div class="flex flex-col dark:text-gray-400">
+                    <div class="mb-3">{hour}:00</div>
+                </div>
+            {/each}
+            <div class="dark:text-gray-400">
+                <div>{end_hour}:00</div>
+            </div>
+        </div>
+        <div class="">
+            {#each arrayRange as _}
+                <div class="flex flex-wrap flex-col mb-0.5">
+                    {#each Array(2) as _}
+                        <div class="bg-gray-200 dark:bg-gray-700 border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-600 rounded overflow-hidden w-16 h-3 m-0.5"></div>
+                    {/each}
+                </div>
+            {/each}
         </div>
     </div>
-</section> -->
+</section>
 
 <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
@@ -51,20 +60,20 @@
                 </div>
                 <div class="mb-6">
                     <Label for="datepicker" class="mb-2">Range</Label>
-                    <Datepicker range />  
+                    <Datepicker range required />  
                 </div>
-                <!-- {#each Array(ranges_no) as _, i}
+                {#each Array(ranges_no) as _, i}
                     <div class="mb-6">
                         <div class="flex justify-between">
                             <Label for="datepicker" class="mb-2">Range {i + 2}</Label>
-                            <button class="text-gray-100 dark:text-gray-400" on:click={() => {ranges_no--}}><XMark></XMark></button>
+                            <CloseButton class="dark:text-gray-200 text-gray-700" on:click={() => {ranges_no--}} />
                         </div>
                         <Datepicker range/>
                     </div>
-                {/each} -->
+                {/each}
                 <div class="mt-8 flex justify-between">
-                    <!-- <Button color="alternative" type="button" on:click={add_range}>Add another range</Button> -->
-                    <Button type="submit" class="mt-4 w-full">Next</Button>
+                    <!-- <Button color="alternative" type="button" on:click={() => {ranges_no++}}>Add another range</Button> -->
+                    <Button type="submit" class="mt-4 w-full" on:click={handleNextButton}>Next</Button>
                 </div>
             </form>
         </div>
