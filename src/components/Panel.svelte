b60cd21297344401c6370370be444f55d852cab8
Bambo <kyrstke@gmail.com>
2023-04-24 20:37:07 +0200

    Added Panels highlighting

--- src/components/Panel.svelte
+++ src/components/Panel.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     let active: boolean = false;
-    $: activeClass = active ? 'bg-red-500 hover:bg-opacity-70 rounded-sm overflow-hidden w-16 h-4 mb-025' : 'bg-gray-200 dark:bg-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 rounded-sm overflow-hidden w-16 h-4 mb-025';
+    $: activeClass = active ? 'bg-red-500 hover:bg-opacity-70' : 'bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600';
 </script>
 
-<button on:click={() => active = !active} class={activeClass}></button>
+<button on:click={() => active = !active} class={activeClass + ' rounded-sm overflow-hidden w-16 h-4 mb-025'}></button>
 
 <style>
     .mb-025 {
