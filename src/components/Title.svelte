38adfe32105cdde7c4df648c358b6c4f85822b1a
Bambo <kyrstke@gmail.com>
2023-07-02 13:32:36 +0200

    Improve layout of main page

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -5,8 +5,8 @@
 </script>
 
 
-<section class="bg-white dark:bg-gray-900">
-  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 flex justify-center">
+<section class="flex justify-center bg-white dark:bg-gray-900">
+  <div class="my-8 mx-4 px-auto max-w-screen-xl lg:mt-16 lg:mx-6 flex justify-center">
     <div class="text-center">
       <Heading tag="h1" class="mb-4">Meeting Scheduler</Heading>
       <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">The best app for scheduling meetings with your team, clients and friends.</P>
