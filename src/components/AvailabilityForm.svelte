fdfbbd1c7193f6f36dadf9fee9106af2c692a094
Bambo <kyrstke@gmail.com>
2023-09-24 20:39:58 +0200

    Export availability form and combined availability

---
+++ src/components/AvailabilityForm.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+    import InputPanel from './InputPanel.svelte';
+    import { Input, Button } from 'flowbite-svelte';
+    import type { PageData } from './$types';
+	import type { DateTimeFormatOptions } from 'luxon';
+    import { redirect } from "@sveltejs/kit";
+    export let data: PageData;
+    
+    function finishEvent(){
+        console.log("Event ended");
+        throw redirect(303, "/summary");
+    }
+
+    function formatDate(date_string: string){
+        const options: DateTimeFormatOptions = { month: 'numeric', day: 'numeric' };
+        return new Date(date_string).toLocaleDateString(undefined, options)
+    }
+
+    let user = "";
+    let start_hour = data.min_hour;
+    let end_hour = data.max_hour;
+    let n_days = data.days.length;
+    let n_panels = (end_hour-start_hour) * 4;
+
+    let available: boolean[][] = new Array(data.days.length)
+                                   .fill(false)
+                                   .map(() => 
+                                     new Array(n_panels).fill(false)
+                                   );
+
+    const arrayRange = () => Array.from(
+            { length: (end_hour - start_hour) },
+            (_, index) => start_hour + index
+        );
+    
+    let showAvForm = true;
+</script>
+
+<form method="POST" action="?/update" class="flex flex-wrap justify-evenly">
+    {#if showAvForm}
+        <div class="flex pt-8 pb-8 px-4 sm:pt-16 sm:px-6">
+            <div class="HOURS-AND-PANELS flex text-sm">
+                <div class="HOURS flex flex-col text-right mr-2 -mt-3 gap-[14.5px]">
+                    {#each arrayRange() as hour}
+                        <div class="">{hour}:00</div>
+                        <div class="">{hour}:30</div>
+                    {/each}
+                    <div class="LAST-HOUR">
+                        <div>{end_hour}:00</div>
+                    </div>
+                </div>
+                <div class="PANELS flex flex-wrap">
+                    {#each Array(n_days) as _, day}
+                        <div class="-mt-7 mr-1 text-center">
+                            <div class="mb-2">{formatDate(data.days[day])}</div>
+                            {#each arrayRange() as hour}
+                                <div id='buttons' class="flex flex-col mb-px last:mb-10">
+                                    {#each Array(4) as _, minute}
+                                        <InputPanel id="{day} {minute} {hour}:TZ" active={available[day][(hour - start_hour) * 4 + minute]} />
+                                    {/each}
+                                </div>
+                            {/each}
+                        </div>
+                    {/each}
+                </div>
+            </div>
+        </div>
+    {/if}
+    <div class="py-6 px-4 sm:py-12 lg:px-6 flex flex-col justify-start text-center gap-1 w-[200px] text-xs lg:w-[250px] sm:text-base">
+        {#if showAvForm}
+            <Input name="user" value={user} placeholder="Name" required></Input>
+            <Button type="submit">Submit availabilty</Button>
+        {/if}
+        <Button on:click={() => showAvForm = !showAvForm} class="mb-2 sm:mb-4">{showAvForm ? "Hide form" : "Show form"}</Button>
+        <Button href="/event/{data.id}/summary">Detailed summary</Button>
+    </div>
+</form>
+
--- src/components/CombinedAvailability.svelte
+++ src/components/CombinedAvailability.svelte
@@ -1,31 +1,50 @@
 <script lang="ts">
     import type { PageData } from './$types';
 	import OutputPanel from './OutputPanel.svelte';
+    import { Heading, P } from 'flowbite-svelte';
+	import type { DateTimeFormatOptions } from 'luxon';
+
     export let data: PageData;
     export let current_hovered_panel: string = "";
 
-    //Function declaration
-    function decToBin(dec: number){
-        return (dec >>> 0).toString(2);
+    $: showDateOnHover = current_hovered_panel != "";
+
+    function sumOnes(bin: string){
+        let sum = 0;
+        for (let i = 0; i < bin.length; i++){
+            if (bin[i] == "1"){
+                sum += 1;
+            }
+        }
+        return sum;
     }
 
-    const sum = (arr: any[]) => {
-        return arr.reduce((total: number, current: string) => {
-            return total + parseInt(current);
+    const max = (arr: number[]) => {
+        return arr.reduce((max: number, current: number) => {
+            return Math.max(max, current);
         }, 0);
     }
 
-    function calculateIntensity(avail: string | any[]){
-        let usersNumber: number;
-        if (data.users.length == 0){
-            usersNumber = 1;
+    const min = (arr: number[]) => {
+        return arr.reduce((min: number, current: number) => {
+            return Math.min(min, current);
+        }, Infinity);
+    }
+
+    function calculateNAvailUsers(avail: string[]) {
+        let n_avail_users: number[] = [];
+        for (let x=0; x<avail.length; x++) {
+            n_avail_users[x] = sumOnes(avail[x]);
         }
-        else{
-            usersNumber = data.users.length;
+        return n_avail_users;
+    }
+
+    function calculateIntensity(n_avail_users: number[], max_n_avail_users: number) {
+        let colorIntensity: number[] = [];
+        for (let x=0; x<n_avail_users.length; x++) {
+            colorIntensity[x] = (n_avail_users[x]/max_n_avail_users) * 0.9 + 0.1;
         }
-        for (let x =0;x<avail.length;x++){
-            summed[x] = (((sum(decToBin(avail[x]).split("")))/usersNumber)*0.8+0.1);
-        }
+        return colorIntensity;
     }
 
     function formatDate(date_string: string){
@@ -33,7 +52,7 @@
         return new Date(date_string).toLocaleDateString(undefined, options)
     }
 
-    function handleMouseEnter(event){
+    function handleMouseEnter(event) {
         current_hovered_panel = event.detail.id;
     }
 
@@ -41,7 +60,24 @@
         current_hovered_panel = "";
     }
 
-    let user = "";
+    function formatDateAndTime(date: Date, hour: number, minute: number){
+        const day = date.getDate();
+        const month = date.getMonth();
+        const year = date.getFullYear();
+        const options: DateTimeFormatOptions = { weekday: 'short', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+        return new Date(year, month, day, hour, minute*15).toLocaleDateString(undefined, options)
+    }
+
+    function calculateIfUserIsAvailable(user: string){
+        let user_index = data.users.indexOf(user);
+        let bin = avail[panel_index];
+
+        if (bin[user_index] == "1") {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     // if data is undefined, set it to empty object
     if (data == undefined){
@@ -57,64 +93,143 @@
 
     let start_hour = data.min_hour;
     let end_hour = data.max_hour;
-    let start_date = data.days[0];
-    let end_date = data.days[data.days.length-1];
-    let days = data.days.length;
-    let hours = (end_hour-start_hour)*4
+    let n_days = data.days.length;
+    let n_panels = (end_hour - start_hour) * 4;
+    let n_users = data.users.length;
 
-    let available: boolean[][] = new Array(data.days.length)
-                                   .fill(false)
-                                   .map(() => 
-                                     new Array(hours).fill(false)
-                                   );
+    $: day = parseInt(current_hovered_panel.split(" ")[0]);
+    $: hour = parseInt(current_hovered_panel.split(" ")[1]);
+    $: minute = parseInt(current_hovered_panel.split(" ")[2]);
+    $: panel_index = day * n_panels + (hour - start_hour) * 4 + minute;
 
     const arrayRange = () => Array.from(
             { length: (end_hour - start_hour) },
             (_, index) => start_hour + index
         );
 
-    let avail: number[] = data.availability;
-    let summed : number[] = [];
-    
-    for (let x =0;x<data.availability.length;x++){
-        summed.push(0);
+    let avail: string[] = data.availability;
+    let n_avail_users: number[] = calculateNAvailUsers(avail);
+
+    const nAvailUsersSet = (n_avail_users: number[]) => {
+        const set = new Set(n_avail_users);
+        console.log('nAvailUsersSet', set);
+        let arr = Array.from(set).sort((a, b) => b - a);
+        console.log('nAvailUsersSet', arr);
+        return arr;
     }
-    
-    calculateIntensity(avail); //Values are not updated after every availability send
+
+    const n_avail_users_set = nAvailUsersSet(n_avail_users);
+
+    let max_n_avail_users = max(n_avail_users_set);
+    let min_n_avail_users = min(n_avail_users_set);
+    console.log('max_n_avail_users', max_n_avail_users);
+    console.log('min_n_avail_users', min_n_avail_users);
+
+    let colorIntensity: number[] = calculateIntensity(n_avail_users, max_n_avail_users);
+
+    const colorIntensitySet = () => {
+        for (let x=0; x<data.availability.length; x++){
+            colorIntensity.push(0);
+        }
+        
+         //Values are not updated after every availability send
+
+        const set = new Set(colorIntensity);
+        console.log('colorIntensity', set);
+        return Array.from(set).sort((a, b) => b - a);
+    }
+
+    const color_intensity_set = colorIntensitySet();
+    console.log('color_intensity_values', color_intensity_set);
 
 </script>
 
-<div class="flex py-8 px-4 lg:py-16 lg:px-6" >
-    <div class="HOURS-AND-PANELS flex text-sm dark:text-gray-400" >
-        <div class="HOURS flex flex-col text-right mr-2 -mt-3 gap-[14.5px]">
-            {#each arrayRange() as hour}
-                    <div class="">{hour}:00</div>
-                    <div class="">{hour}:30</div>
-            {/each}
-            <div class="LAST-HOUR ">
-                <div>{end_hour}:00</div>
+<div class="flex flex-wrap justify-evenly">   
+    <div class="COMBINED AVAILABILITY flex pb-8 py-16 px-4 lg:py-16 lg:px-6">
+        <div class="HOURS-AND-PANELS flex text-sm">
+            <div class="HOURS flex flex-col text-right mr-2 -mt-3 gap-[14.5px]">
+                {#each arrayRange() as hour}
+                        <div class="">{hour}:00</div>
+                        <div class="">{hour}:25</div>
+                {/each}
+                <div class="LAST-HOUR">
+                    <div>{end_hour}:00</div>
+                </div>
+            </div>
+            <div class="PANELS flex flex-wrap">
+                {#each Array(n_days) as _, day}
+                    <div class="-mt-7 mr-1 text-center">
+                        <div class="mb-2">{formatDate(data.days[day])}</div>
+                        {#each arrayRange() as hour}
+                            <div id='buttons' class="flex flex-col mb-px last:mb-10">
+                                {#each Array(4) as _, minute}
+                                    <OutputPanel 
+                                        id="{day} {hour} {minute}"
+                                        on:mouse_enter={handleMouseEnter}
+                                        on:mouse_leave={handleMouseLeave}
+                                        custom_style="background-color:rgba(0, 175, 0, 
+                                        {colorIntensity[minute+4*(hour-start_hour)+n_panels*day]})"
+                                    />
+                                {/each}
+                            </div>
+                        {/each}
+                    </div>
+                {/each}
             </div>
         </div>
-        <div class="PANELS flex">
-            {#each Array(days) as _, day}
-                <div class="-mt-7 mr-1 text-center">
-                    <div class="mb-2">{formatDate(data.days[day])}</div>
-                    {#each arrayRange() as hour}
-                        <div id='buttons' class="flex flex-col mb-px">
-                            {#each Array(4) as _, minute}
-                                <OutputPanel 
-                                    id="{day} {hour} {minute}"
-                                    on:mouse_enter={handleMouseEnter}
-                                    on:mouse_leave={handleMouseLeave}
-                                    custom_style="background-color:rgba(0, 175, 0, 
-                                    {summed[minute+4*(hour-start_hour)+hours*day]})"
-                                />
-                            {/each}
-                        </div>
-                    {/each}
+    </div>
+
+    <div class="QUICK SUMMARY px-2 py-8 flex flex-col text-center lg:px-8 w-[20rem] lg:w-[25rem]">
+        {#if showDateOnHover}
+            <Heading tag="h5" class="pb-8 w-[20rem] lg:w-[25rem]">{formatDateAndTime(data.days[day], hour, minute)}</Heading>
+            <!-- <Heading tag="h5">{day} {hour} {minute}</Heading> -->
+            <div class="flex justify-evenly w-[20rem] lg:w-[25rem] text-white text-base">
+                <div class="">
+                    <Heading tag="h6">Available</Heading>
+                    <ul>
+                        {#each data.users as user}
+                            {#if current_hovered_panel != "" && calculateIfUserIsAvailable(user)}
+                                <li>{user}</li>
+                            {/if}
+                        {/each}
+                    </ul>
                 </div>
-            {/each}
-        </div>
+                <div>
+                    <Heading tag="h6">Unavailable</Heading>
+                    <ul>
+                        {#each data.users as user}
+                            {#if current_hovered_panel != "" && !calculateIfUserIsAvailable(user)}
+                                <li>{user}</li>
+                            {/if}
+                        {/each}
+                    </ul>
+                </div>
+            </div>
+        {:else}
+            <div class="flex flex-col gap-2 pb-4">
+                <Heading tag="h5">Legend</Heading>
+                <div class="text-sm text-gray-400">Mouseover a panel to see who is available</div>
+            </div>
+            <!-- <Heading tag="h5">{day} {hour} {minute}</Heading> -->
+                <!-- <Heading tag="h5" class="">Legend</Heading> -->
+                <!-- Legend -->
+            <div class="flex justify-center">
+                <P class="mr-2 text-center">
+                    {max_n_avail_users}/{n_users} available
+                </P>
+                {#each color_intensity_set as color}
+                    {#if color != 0}
+                        <div class="w-5 h-5 rounded-sm mr-px my-auto" style="background-color:rgba(0, 175, 0, {color})"></div>
+                    {/if}
+                {/each}
+                {#if color_intensity_set.length !== 1}
+                    <P class="ml-2 text-center">
+                        {min_n_avail_users}/{n_users} available
+                    </P>
+                {/if}
+            </div>
+        {/if}
     </div>
 </div>
+
 
